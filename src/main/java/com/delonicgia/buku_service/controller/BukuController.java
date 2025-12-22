0f20d81dec7fe6a79aa5fcd3aed59ace03776d8e
Delonicligia <delonicligia@gmail.com>
2025-12-22 21:54:49 +0700

    memperbaiki buku service

--- src/main/java/com/delonicgia/buku_service/controller/BukuController.java
+++ src/main/java/com/delonicgia/buku_service/controller/BukuController.java
@@ -1,50 +1,56 @@
 package com.delonicgia.buku_service.controller;
 
-
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.delonicgia.buku_service.model.Buku;
-import com.delonicgia.buku_service.service.BukuCommandService;
-import com.delonicgia.buku_service.service.BukuQueryService;
+import com.delonicgia.buku_service.service.BukuService;
 
 @RestController
 @RequestMapping("/api/buku")
 public class BukuController {
 
-    private final BukuCommandService commandService;
-    private final BukuQueryService queryService;
-
-    public BukuController(BukuCommandService commandService, BukuQueryService queryService) {
-        this.commandService = commandService;
-        this.queryService = queryService;
-    }
+    @Autowired
+    private BukuService bukuService;
 
     // GET semua buku
     @GetMapping
     public List<Buku> getAllBuku() {
-        return queryService.semuaBuku();
+        return bukuService.getAllBuku();
     }
 
     // GET buku by ID
     @GetMapping("/{id}")
     public ResponseEntity<Buku> getBukuById(@PathVariable Long id) {
-        return queryService.getBukuById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+        Buku buku = bukuService.getBukuById(id);
+        return (buku != null)
+                ? ResponseEntity.ok(buku)
+                : ResponseEntity.notFound().build();
     }
 
     // POST buat buku baru
     @PostMapping
     public Buku createBuku(@RequestBody Buku buku) {
-        return commandService.tambahBuku(buku);
+        return bukuService.createBuku(buku);
+    }
+
+    // PUT / UPDATE buku
+    @PutMapping("/{id}")
+    public ResponseEntity<Buku> updateBuku(@PathVariable Long id, @RequestBody Buku dataBaru) {
+        try {
+            Buku updated = bukuService.updateBuku(id, dataBaru);
+            return ResponseEntity.ok(updated);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     // DELETE buku by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteBuku(@PathVariable Long id) {
-        commandService.hapusBuku(id);
+        bukuService.deleteBuku(id);
         return ResponseEntity.ok().build();
     }
 }
