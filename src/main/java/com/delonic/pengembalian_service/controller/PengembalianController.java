6d4262202ec030d7435d52b18845218f004a8a3e
Delonicligia <delonicligia@gmail.com>
2026-01-18 20:42:33 +0700

    menambahkan log untuk pengembalian service

--- src/main/java/com/delonic/pengembalian_service/controller/PengembalianController.java
+++ src/main/java/com/delonic/pengembalian_service/controller/PengembalianController.java
@@ -17,37 +17,48 @@ import com.delonic.pengembalian_service.model.Pengembalian;
 import com.delonic.pengembalian_service.service.PengembalianService;
 import com.delonic.pengembalian_service.vo.ResponseTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/pengembalian")
 public class PengembalianController {
     @Autowired
     private PengembalianService pengembalianService;
 
+    // GET semua pengembalian
     @GetMapping
     public List<Pengembalian> getPengembalians(){
+        log.info("Berhasil mengambil semua pengembalian");
         return pengembalianService.getAllPengembalians();
     }
 
+    // GET pengembalian by ID
     @GetMapping("/{id}")
     public ResponseEntity<Pengembalian> getPengembalianById(@PathVariable Long id) {
+        log.info("Berhasil mendapatkan pengembalian berdasarkan id");
         Pengembalian pengembalian = pengembalianService.getPengembalianById(id);
         return pengembalian != null ? ResponseEntity.ok(pengembalian): ResponseEntity.notFound().build();
     }
 
     @GetMapping("/{id}/detail")
 public ResponseEntity<List<ResponseTemplate>> getPengembalianDetail(@PathVariable Long id) {
+    log.info("Berhasil mendapatkan detail pengembalian berdasarkan id");
     List<ResponseTemplate> response = pengembalianService.getPengembalianWithDetailById(id);
     return response != null ? ResponseEntity.ok(response) : ResponseEntity.notFound().build();
 }
 
-
+// POST buat pengembalian baru
     @PostMapping
     public Pengembalian createPengembalian(@RequestBody Pengembalian pengembalian) throws ParseException{
+        log.info("Create Pengembalian : {}", pengembalian.getPeminjamanId());
         return pengembalianService.createPengembalian(pengembalian);
     }
 
+    // DELETE pengembalian by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletePengembalian(@PathVariable Long id){
+        log.info("Berhasil menghapus pengembalian");
         pengembalianService.deletePengembalian(id);
         return ResponseEntity.ok().build();
     }
